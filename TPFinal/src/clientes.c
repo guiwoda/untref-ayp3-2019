94da34a84b077600be0daaa9642962a89213a8aa
Guido Contreras Woda <gcontreras@medallia.com>
2019-06-30 21:57:11 -0300

    Comentarios en header. Agrego struct Clientes para recorrer resultados

--- TPFinal/src/clientes.c
+++ TPFinal/src/clientes.c
@@ -1,21 +1,11 @@
 #include "clientes.h"
 
-unsigned int CLIENTES_LAST_ID = 0;
-
+static unsigned int CLIENTES_LAST_ID = 0;
 unsigned int cliente_proximo_id() {
     return ++CLIENTES_LAST_ID;
 }
 
-/**
- * Agrega un cliente.
- *
- * @param nombre
- * @param nacimiento
- * @param referente_id
- *
- * @return el cliente agregado.
- */
-Cliente* cliente_referido(char *nombre, time_t nacimiento, int referente_id) {
+Cliente* cliente_nuevo_con_referido(char *nombre, time_t nacimiento, int referente_id) {
     Cliente *cliente = (Cliente*) malloc(sizeof(Cliente));
     cliente->id = cliente_proximo_id();
     cliente->nombre = nombre;
@@ -26,26 +16,17 @@ Cliente* cliente_referido(char *nombre, time_t nacimiento, int referente_id) {
 }
 
 Cliente* cliente_nuevo(char *nombre, time_t nacimiento) {
-    return cliente_referido(nombre, nacimiento, 0);
+    return cliente_nuevo_con_referido(nombre, nacimiento, 0);
 }
 
 Cliente* cliente_indice(int id) {
     return NULL;
 }
 
-Cliente* cliente_buscar_nombre(char *busqueda) {
+Clientes* cliente_buscar_nombre(char *busqueda) {
     return NULL;
 }
 
-Cliente* cliente_buscar_edad_rango(int min, int max) {
+Clientes* cliente_buscar_edad_rango(int min, int max) {
     return NULL;
 }
-
-Cliente* cliente_buscar_edad_mayor(int min) {
-    return cliente_buscar_edad_rango(min, INT_MAX);
-}
-
-Cliente* cliente_buscar_edad_menor(int max) {
-    return cliente_buscar_edad_rango(0, max);
-}
-
--- TPFinal/src/clientes.h
+++ TPFinal/src/clientes.h
@@ -5,20 +5,66 @@
 #include <limits.h>
 #include <time.h>
 
-typedef struct _cliente {
+typedef struct {
     unsigned int id;
     char *nombre;
     time_t nacimiento;
     unsigned int referente_id;
 } Cliente;
 
+typedef struct {
+    Cliente *prev;
+    Cliente *current;
+    Cliente *next;
+} Clientes;
+
+/**
+ * @return el próximo ID a usar para un cliente.
+ */
 unsigned int cliente_proximo_id();
-Cliente* cliente_referido(char *nombre, time_t nacimiento, int referente_id);
+
+/**
+ * Agrega un cliente con referido.
+ *
+ * @param nombre
+ * @param nacimiento
+ * @param referente_id
+ *
+ * @return el cliente agregado.
+ */
+Cliente* cliente_nuevo_con_referido(char *nombre, time_t nacimiento, int referente_id);
+
+/**
+ * Agrega un cliente.
+ *
+ * @param nombre
+ * @param nacimiento
+ *
+ * @return el cliente agregado.
+ */
 Cliente* cliente_nuevo(char *nombre, time_t nacimiento);
+
+/**
+ * Busca un cliente en el índice.
+ * @param id
+ * @return el cliente, si existe, o {@code NULL}.
+ */
 Cliente* cliente_indice(int id);
-Cliente* cliente_buscar_nombre(char *busqueda);
-Cliente* cliente_buscar_edad_rango(int min, int max);
-Cliente* cliente_buscar_edad_mayor(int min);
-Cliente* cliente_buscar_edad_menor(int max);
+
+/**
+ * Busca clientes por nombre.
+ * @param busqueda
+ * @return los clientes encontrados que concuerden con la búsqueda.
+ */
+Clientes* cliente_buscar_nombre(char *busqueda);
+
+/**
+ * Busca clientes en un rango de edad.
+ *
+ * @param min desde qué edad.
+ * @param max hasta qué edad.
+ * @return los clientes encontrados que concuerden con la búsqueda.
+ */
+Clientes* cliente_buscar_edad_rango(int min, int max);
 
 #endif //TPFINAL_CLIENTES_H
